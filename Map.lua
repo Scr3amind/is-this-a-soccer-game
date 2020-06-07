a8fdf587ff12316eb30c843e29070f4a4e34565d
scr3amind <screamindarko@gmail.com>
2020-06-06 22:42:32 -0500

    Cleaning Map code

--- Map.lua
+++ Map.lua
@@ -2,42 +2,43 @@ Map = Class{}
 
 function Map:init(width, height)
     
-    self.width = width
-    self.height = height
     self.offsetY = 0
     self.timer = 0
+    self.blocks_width = 50
     
-    upper = Block(0, 0, width, 50)
-    ground = Block(0, height - 50, width, 50)
+    -- Map Blocks
+    self.upper = Block(0, 0, width, self.blocks_width)
+    self.ground = Block(0, height - 50, width, self.blocks_width)
     
-    wall_left_up = Block(0, 0 -  height * 2 / 3, 50, height)
-    wall_left_down = Block(0, height * 2 / 3, 50, height)
-    wall_right_up = Block(width - 50, 0, 50, height)
-    wall_right_down = Block(width - 50, height * 2 / 3, 50, height)
+    self.wall_left_up = Block(0, 0 -  height * 2 / 3, self.blocks_width, height)
+    self.wall_left_down = Block(0, height * 2 / 3, self.blocks_width, height)
+    self.wall_right_up = Block(width - self.blocks_width, 0, self.blocks_width, height)
+    self.wall_right_down = Block(width - self.blocks_width, height * 2 / 3, self.blocks_width, height)
     
-    left_limit = Block(-80 , 0, 50, height)
-    right_limit = Block(width + 30 , 0, 50, height)
+    self.left_limit = Block(-80 , 0, 50, height)
+    self.right_limit = Block(width + 30 , 0, 50, height)
     
-    self.height1 = wall_left_up.collider:getY()
-    self.height2 = wall_left_down.collider:getY()
+
+    self.height1 = self.wall_left_up.collider:getY()
+    self.height2 = self.wall_left_down.collider:getY()
 end
 
 function Map:update(dt)
     self.timer = self.timer + dt
     self.offsetY = math.sin(self.timer)
-    wall_left_up.collider:setY(self.height1 + self.offsetY*150)
-    wall_left_down.collider:setY(self.height2 + self.offsetY*150)
-    wall_right_up.collider:setY(self.height1 + 1 - self.offsetY*150)
-    wall_right_down.collider:setY(self.height2 + 1 - self.offsetY*150)
+    self.wall_left_up.collider:setY(self.height1 + self.offsetY*150)
+    self.wall_left_down.collider:setY(self.height2 + self.offsetY*150)
+    self.wall_right_up.collider:setY(self.height1 + 1 - self.offsetY*150)
+    self.wall_right_down.collider:setY(self.height2 + 1 - self.offsetY*150)
     
    
 end
 
 function Map:render()
-    upper:render()
-    ground:render()
-    wall_left_up:render()
-    wall_left_down:render()
-    wall_right_up:render()
-    wall_right_down:render()
+    self.upper:render()
+    self.ground:render()
+    self.wall_left_up:render()
+    self.wall_left_down:render()
+    self.wall_right_up:render()
+    self.wall_right_down:render()
 end
